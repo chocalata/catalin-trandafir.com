bf163bffa447e0d8131384c8ee945b2a36acc2f2
Catalin Trandafir <cata627@gmail.com>
2024-08-01 21:15:39 +0200

    Prepared to deploy on server with puppeteer working

--- first-web/main-page/src/routes/root_routes.js
+++ first-web/main-page/src/routes/root_routes.js
@@ -1,33 +1,36 @@
 module.exports = function routes(log) {
-    const router = require('express').Router();
+	const router = require("express").Router();
 
-    function testCheck (req, res, next){
-        log.info("testCheck()");
-        log.info(req.body)
-        next();
-    }
+	function testCheck(req, res, next) {
+		log.info("testCheck()");
+		log.info(req.body);
+		next();
+	}
 
-    router.get('/index', testCheck, async function(req, res) {
-        log.info("PATH: /first-web/index")
-        res.render('index')
-    })
+	router.get("/index", testCheck, async function (req, res) {
+		log.info("PATH: /first-web/index");
+		res.render("index");
+	});
 
-    router.get('/contacte', testCheck, async function(req, res) {
-        log.info("PATH: /first-web/contacte")
-        res.render('contacte')
-    })
-    
-    router.get('/references', testCheck, async function(req, res) {
-        log.info("PATH: /first-web/references")
-        res.render('references')
-    })
-    
-    router.post('/form_result', testCheck, async function(req, res) {
-        log.info("PATH: /first-web/form_result")
-        console.log(req.body)
-        res.render('form_result', {form_data: req.body})
-    })
-    
+	router.get("/contacte", testCheck, async function (req, res) {
+		log.info("PATH: /first-web/contacte");
+		res.render("contacte");
+	});
 
-    return router;
-}
+	router.get("/references", testCheck, async function (req, res) {
+		log.info("PATH: /first-web/references");
+		res.render("references");
+	});
+
+	router.post("/form_result", testCheck, async function (req, res) {
+		log.info("PATH: /first-web/form_result");
+		console.log(req.body);
+		res.render("form_result", { form_data: req.body });
+	});
+
+	router.all("/*", (req, res) => {
+		res.redirect("/first-web/index");
+	});
+
+	return router;
+};
