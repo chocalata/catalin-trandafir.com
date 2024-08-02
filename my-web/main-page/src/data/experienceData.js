5dfd3683a62262508bf11f7edaf19146e6f6882e
Catalin Trandafir <cata627@gmail.com>
2024-08-02 16:32:41 +0200

    Some refactoring and skipping the puppeteer chromium download

---
+++ my-web/main-page/src/data/experienceData.js
@@ -0,0 +1,106 @@
+workData = [
+	{
+		position: "left",
+		company_name: "NTT Data",
+		company_url: "https://es.nttdata.com/",
+		work_description:
+			"Java Developer specialized in task automation and system maintenance, contributing within an internal support team. Creator of efficient solutions to streamline processes and enhance system stability through Java development.",
+		technologies: [
+			{
+				name: "Java",
+			},
+		],
+	},
+	{
+		position: "right",
+		company_name: "PUE",
+		company_url: "https://pue.es/",
+		work_description:
+			"Performing the role of Full-stack and Systems Administrator, providing efficient solutions for the development and maintenance of systems. I handle both front-end and back-end, along with effective infrastructure management to ensure optimal performance.",
+		technologies: [
+			{
+				name: "Docker",
+			},
+			{
+				name: "Python",
+			},
+			{
+				name: "Node.js",
+			},
+			{
+				name: "Express.js",
+			},
+			{
+				name: "MySQL",
+			},
+			{
+				name: "Redis",
+			},
+			{
+				name: "Linux",
+			},
+			{
+				name: "HTML5",
+			},
+		],
+	},
+	{
+		position: "right",
+		company_name: "I+D+I PUE",
+		company_url: "https://pue.es/",
+		work_description:
+			"Additionally, I have taken on the position of Researcher in Research, Development, and Innovation (I+D+I), contributing to the exploration of new technologies and methodologies to enhance our projects and stay at the forefront of innovation. I continue to fulfill the role of Full-stack and Systems Administrator.",
+		technologies: [
+			{
+				name: "Kubernetes",
+			},
+			{
+				name: "AWS",
+			},
+			{
+				name: "Docker",
+			},
+			{
+				name: "Python",
+			},
+			{
+				name: "Node.js",
+			},
+			{
+				name: "Express.js",
+			},
+			{
+				name: "MySQL",
+			},
+			{
+				name: "Redis",
+			},
+			{
+				name: "Linux",
+			},
+			{
+				name: "HTML5",
+			},
+		],
+	},
+];
+
+projectData = [
+	{
+		title: "Apolo X",
+		description:
+			"This was my first project. It's a videogame with a single player and local multiplayer mode. Made as a final project for the course with Alberto Gutierro.",
+		github: "https://github.com/Alberto-Gutierro/Catalin_Alberto-Juego_Naves",
+		image: "/img/projects/ApoloX.png",
+		technologies: [
+			{
+				name: "Java",
+			},
+		],
+	},
+];
+
+module.exports = {
+	workData,
+	projectData,
+};
