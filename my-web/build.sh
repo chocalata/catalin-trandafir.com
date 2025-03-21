8ff5f36e172a70b4beac889c0b9804d1e9dccfd1
chocalata <37223556+chocalata@users.noreply.github.com>
2025-03-21 17:49:28 +0100

    Delete my-web directory

--- my-web/build.sh
+++
@@ -1,4 +0,0 @@
-
-docker build ./main-page -t chocalata/catalin-trandafir.com:my-web_2024-11-08-v1
-
-docker push chocalata/catalin-trandafir.com:my-web_2024-11-08-v1
--- my-web/main-page/src/whatsapp-data/README.md
+++
@@ -1,3 +0,0 @@
-# whatsapp-web.js
-
-This is a folder for the whatsapp web session and the web cache that is using `whatsapp-web.js`
