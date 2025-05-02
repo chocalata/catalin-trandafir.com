79012bb4d5f9d4a090f3ced91705d7c3752ba099
chocalata <cata627@gmail.com>
2025-05-02 17:12:21 +0200

    New projeect added

--- portfolio/build.sh
+++ portfolio/build.sh
@@ -1,4 +1,4 @@
 
-docker build ./main-page -t chocalata/catalin-trandafir.com:my-web_2025-04-02-v1
+docker build ./main-page -t chocalata/catalin-trandafir.com:my-web_2025-05-02-v1
 
-docker push chocalata/catalin-trandafir.com:my-web_2025-04-02-v1
+docker push chocalata/catalin-trandafir.com:my-web_2025-05-02-v1
