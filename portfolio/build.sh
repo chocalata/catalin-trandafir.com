29685a0aec964eeb953913eacf88fcb027322b22
chocalata <cata627@gmail.com>
2025-09-26 17:44:24 +0200

    Implement WhatsApp message handling and versioning in build script; add message storage

--- portfolio/build.sh
+++ portfolio/build.sh
@@ -1,4 +1,64 @@
 
-docker build ./main-page -t chocalata/catalin-trandafir.com:my-web_2025-05-02-v1
+# docker build ./main-page -t chocalata/catalin-trandafir.com:my-web_2025-05-02-v1
 
-docker push chocalata/catalin-trandafir.com:my-web_2025-05-02-v1
+# docker push chocalata/catalin-trandafir.com:my-web_2025-05-02-v1
+
+app="my-web"
+repository="chocalata/catalin-trandafir.com"
+
+processes=("my-web")
+
+echo "Choose a process to build:"
+select process in "${processes[@]}"; do
+  if [ -n "$process" ]; then
+    break
+  else
+    echo "Invalid selection. Try again."
+  fi
+done
+
+if [ -z "$process" ]; then
+  echo "No process selected. Exiting..."
+  exit 1
+fi
+
+# Set the folder and dockerfile for the selected process
+if [ "$process" == "my-web" ]; then
+  folder="./main-page/"
+  dockerfile="Dockerfile"
+
+else
+  echo "Invalid process. Exiting..."
+  exit 1
+fi
+
+last_version=$(grep $process version)
+
+last_version_date=$(echo $last_version | cut -d' ' -f2 | cut -d'-' -f 1-3)
+version_date=$(echo $last_version | cut -d' ' -f2 | cut -d'-' -f 1-3)
+last_version_number=$(echo $last_version | cut -d' ' -f2 | cut -d'-' -f 4- | cut -d'v' -f 2)
+
+today=$(date +"%Y-%m-%d")
+
+if [ "$last_version_date" != "$today" ]; then
+  version_date=$today
+  version_number=1
+else
+  version_number=$((last_version_number+1))
+fi
+
+new_version="$version_date-v$version_number"
+
+echo "Building $app"_"$new_version..."
+
+# update the version file (if not exists, create it, if exists, update it with sed)
+if [ -z "$last_version" ]; then
+  echo "$process $new_version" >> version
+else
+  sed -i "s/$last_version/$process $new_version/g" version
+fi
+
+# Build the image
+docker build -t "$repository:$app"_"$new_version" -f $folder$dockerfile $folder
+
+docker push "$repository:$app"_"$new_version"
--- portfolio/main-page/src/models/WhatsAppController.js
+++ portfolio/main-page/src/models/WhatsAppController.js
@@ -1,67 +1,113 @@
 /**
  * This is the class that will handle the WhatsApp API
  */
-const { Client, LocalAuth } = require("whatsapp-web.js");
-const QRCode = require("qrcode");
+const { Client, LocalAuth } = require('whatsapp-web.js')
+const QRCode = require('qrcode')
+const path = require('path')
+const fs = require('fs')
 
-const path = require("path");
+const messagesToSendFilePath = path.join(
+  __dirname,
+  '../whatsapp-data/messagesToSend.txt'
+)
 
-const WHATSAPP_CHAT_WEB_CONTACT = process.env.WHATSAPP_CHAT_WEB_CONTACT;
+const WHATSAPP_CHAT_WEB_CONTACT = process.env.WHATSAPP_CHAT_WEB_CONTACT
 
 class WhatsAppController {
-  #client;
-  #log;
-  #clientIsReady = false;
+  #client
+  #log
+  #clientIsReady = false
   constructor(log) {
     const client = new Client({
       puppeteer: {
         args: [
-          "--no-sandbox",
-          "--disable-setuid-sandbox",
-          "--disable-extensions",
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-extensions'
         ],
-        headless: true,
+        headless: true
       },
       authStrategy: new LocalAuth({
-        dataPath: path.join(__dirname, "../whatsapp-data/"),
+        dataPath: path.join(__dirname, '../whatsapp-data/')
       }),
       webVersionCache: {
-        type: "local",
-        path: path.join(__dirname, "../whatsapp-data/cache/"),
-      },
-    });
+        type: 'local',
+        path: path.join(__dirname, '../whatsapp-data/cache/')
+      }
+    })
 
-    client.initialize();
+    client.initialize()
 
-    client.on("qr", (qr) => {
-      log.info(`QR RECEIVED: ${qr}`);
-      QRCode.toString(qr, { type: "terminal" }, function (err, url) {
-        console.log(url);
-      });
-    });
+    client.on('qr', (qr) => {
+      log.info(`QR RECEIVED: ${qr}`)
+      QRCode.toString(qr, { type: 'terminal' }, function (err, url) {
+        console.log(url)
+      })
+    })
 
-    client.on("ready", () => {
-      log.info("CLIENT IS READY!");
-      this.#clientIsReady = true;
+    client.on('ready', () => {
+      log.info('CLIENT IS READY!')
+      this.#clientIsReady = true
 
-      this.sendMessage("Whatsapp client ready!");
-    });
+      this.sendMessage('Whatsapp client ready!')
 
-    this.#client = client;
-    this.#log = log;
+      this.sendRemainingMessages()
+    })
+
+    this.#client = client
+    this.#log = log
   }
 
   sendMessage(message) {
     if (!this.#clientIsReady) {
-      this.#log.error("Client is not ready yet!");
-      return false;
+      this.#log.error('Client is not ready yet!')
+      return false
     }
     this.#client.getChatById(WHATSAPP_CHAT_WEB_CONTACT).then((chat) => {
       chat.sendMessage(message).then((msg) => {
-        this.#log.info(`Message sent: ${msg.body}`);
-      });
-    });
+        this.#log.info(`Message sent: ${msg.body}`)
+      })
+    })
+  }
+
+  getMessagesToSendFilePath() {
+    return messagesToSendFilePath
+  }
+
+  getMessagesToSend() {
+    try {
+      const data = fs.readFileSync(messagesToSendFilePath, 'utf-8')
+      return data
+        .split('\n||||||||||||||||||||||||||||||||||||||||||||||')
+        .filter((msg) => msg.trim() !== '')
+    } catch (err) {
+      this.#log.error('Error reading messagesToSend.txt:', err)
+      return []
+    }
+  }
+
+  clearMessagesToSend() {
+    try {
+      fs.writeFileSync(messagesToSendFilePath, '')
+      this.#log.info('messagesToSend.txt cleared successfully.')
+      return true
+    } catch (err) {
+      this.#log.error('Error clearing messagesToSend.txt:', err)
+      return false
+    }
+  }
+
+  sendRemainingMessages() {
+    const messages = this.getMessagesToSend()
+    messages.forEach((message) => {
+      this.sendMessage(message)
+    })
+    this.clearMessagesToSend()
+  }
+
+  isClientReady() {
+    return this.#clientIsReady
   }
 }
 
-module.exports = WhatsAppController;
+module.exports = WhatsAppController
